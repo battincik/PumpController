01956755a086fa333824031a6d55ea153b9ff762
Saamaan <saamaan@kinsolresearch.com>
2015-06-23 16:33:09 -0700

    Minor update to the kernel module build script.
    Replaced obsolete ia32-libs package with new ones.

--- drivers/pump_driver/prep.bash
+++ drivers/pump_driver/prep.bash
@@ -5,7 +5,7 @@ export CCPREFIX=/home/saamaan/ccMadness/rpi-tool-master/arm-bcm2708/arm-bcm2708-
 #zcat ../config.gz > .config
 #make ARCH=arm CROSS_COMPILE=${CCPREFIX} oldconfig
 
-#sudo apt-get install ia32-libs
+#sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0
 #make ARCH=arm CROSS_COMPILE=${CCPREFIX}
 
 
