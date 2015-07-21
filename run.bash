d9bc80d9c65f14e79c3c8d0242f67bd79d7e4082
Saamaan <saamaan@kinsolresearch.com>
2015-07-21 23:41:45 +0000

    Quick and dirty fix to redirect logs to files.
    TODO: do this properly by using a library logger (path and files names
    to be taken from config file).
    Also added modified couchdb init script (sends a signal on start to
    pump-controller script).

--- run.bash
+++ run.bash
@@ -5,8 +5,21 @@
 # which perl
 # echo "$@"
 # perl "$@"
+OUT=""
+for i in $@; do
+	case $i in
+		--output=*)
+			OUT=" > ${i#*=}"
+			;;
+		*)
+			echo "Unrecognized option: $i"
+			exit 2
+			;;
+	esac
+done
+echo "--output switch: $OUT"
 sleep 10
-/home/saamaan/perl5/perlbrew/perls/perl-5.16.3/bin/perl /home/saamaan/CowichanEnergy/lib/kickStart.perl --terms='LCD&realKP' --coldstart
+/home/saamaan/perl5/perlbrew/perls/perl-5.16.3/bin/perl /home/saamaan/CowichanEnergy/lib/kickStart.perl --terms='LCD&realKP' --coldstart $OUT
 # PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 sudo rmmod pump_driver
 sleep 2
